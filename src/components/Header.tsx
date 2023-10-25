5ced937227bf45e654bf676427e9034f97575cff
Richard Boomsma <spaaacetoast@proton.me>
2023-10-26 01:55:07 +0200

    removed scrollRestoration and added headerMenu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import { createSignal, onMount } from "solid-js";
 import { isServer } from "solid-js/web";
 import { lenis } from "src/constants/lenis";
-import { css } from "styled-system/css";
 import { Flex, panda } from "styled-system/jsx";
+import { HeaderMenu } from "./HeaderMenu";
 import { WideContainer } from "./ui/WideContainer";
 
 const topThreshold = 100;
@@ -10,6 +10,7 @@ const isMobile = () => window.innerWidth < 768;
 
 export const Header = () => {
   const [isAtTop, setAtTop] = createSignal(false);
+  const [isMenuOpen, setMenuOpen] = createSignal(false);
   let headerLinks: HTMLAnchorElement[] = Array.from({ length: 3 });
 
   onMount(() => {
@@ -19,6 +20,7 @@ export const Header = () => {
       window.onscroll = () => {
         var currentPosition = document.documentElement.scrollTop;
         const atTop = currentPosition < topThreshold;
+
         if (isAtTop() !== atTop) {
           setAtTop(atTop);
         }
@@ -27,46 +29,34 @@ export const Header = () => {
   });
 
   return (
-    <panda.div position="fixed" zIndex={10} width="100%">
-      <WideContainer>
-        <Flex
-          justifyContent="space-between"
-          alignItems="center"
-          paddingTop="6"
-          fontFamily="noto-mono"
-        >
-          <panda.a
-            display="flex"
-            fontSize="3rem"
-            fontWeight={800}
-            letterSpacing="-0.2rem"
-            marginTop="-1"
-            href="#"
-            onclick={() => lenis?.scrollTo(0, { lerp: 0 })}
+    <>
+      <HeaderMenu isOpen={isMenuOpen()} onClose={() => setMenuOpen(false)} />
+      <panda.div position="fixed" zIndex={10} width="100%">
+        <WideContainer>
+          <Flex
+            justifyContent="space-between"
+            alignItems="center"
+            paddingTop="6"
+            fontFamily="noto-mono"
           >
-            <panda.div display="inline-block">o</panda.div>
-            {["b", "s", "c", "u", "r", "e"].map((letter, i) => (
-              <panda.div
-                display="inline-block"
-                overflow="hidden"
-                transform={
-                  !isServer && isAtTop() ? "translateY(0%)" : "translateY(-50%)"
-                }
-                transition="transform"
-                transitionTimingFunction="cubic-bezier(.86,0,.07,1)"
-                transitionDuration="1s"
-                style={{
-                  "transition-delay": `${i * 0.05}s`,
-                }}
-                paddingRight="2"
-                marginRight="-2"
-              >
+            <panda.a
+              display="flex"
+              fontSize="3rem"
+              fontWeight={800}
+              letterSpacing="-0.2rem"
+              marginTop="-1"
+              href="#"
+              onclick={() => lenis?.scrollTo(0, { lerp: 0 })}
+            >
+              <panda.div display="inline-block">o</panda.div>
+              {["b", "s", "c", "u", "r", "e"].map((letter, i) => (
                 <panda.div
                   display="inline-block"
+                  overflow="hidden"
                   transform={
                     !isServer && isAtTop()
                       ? "translateY(0%)"
-                      : "translateY(110%)"
+                      : "translateY(-50%)"
                   }
                   transition="transform"
                   transitionTimingFunction="cubic-bezier(.86,0,.07,1)"
@@ -74,72 +64,96 @@ export const Header = () => {
                   style={{
                     "transition-delay": `${i * 0.05}s`,
                   }}
+                  paddingRight="2"
+                  marginRight="-2"
                 >
-                  {letter}
+                  <panda.div
+                    display="inline-block"
+                    transform={
+                      !isServer && isAtTop()
+                        ? "translateY(0%)"
+                        : "translateY(110%)"
+                    }
+                    transition="transform"
+                    transitionTimingFunction="cubic-bezier(.86,0,.07,1)"
+                    transitionDuration="1s"
+                    style={{
+                      "transition-delay": `${i * 0.05}s`,
+                    }}
+                  >
+                    {letter}
+                  </panda.div>
                 </panda.div>
-              </panda.div>
-            ))}
-          </panda.a>
-          <Flex
-            gap={12}
-            position="relative"
-            overflow="hidden"
-            class={css({
-              "& > a": {
-                visibility: { base: "hidden", md: "visible" },
-                transition: "all",
-                transitionDuration: "1s",
-                transform: isAtTop() ? `translateY(0%)` : `translateY(-150%)`,
-                transitionTimingFunction: "cubic-bezier(.86,0,.07,1)",
-              },
-              "& > button": {
-                transition: "all",
-                transitionDuration: "1s",
-                transform:
-                  isServer || (isAtTop() && !isMobile())
-                    ? `translateY(150%)`
-                    : `translateY(0%)`,
-                transitionTimingFunction: "cubic-bezier(.86,0,.07,1)",
-              },
-            })}
-          >
-            <panda.a
-              ref={headerLinks[0]}
-              href="#about"
-              onclick={() => lenis?.scrollTo("#about", { lerp: 0 })}
-            >
-              ABOUT
+              ))}
             </panda.a>
-            <panda.a
-              ref={headerLinks[1]}
-              href="#experience"
-              onclick={() => lenis?.scrollTo("#experience", { lerp: 0 })}
-              transitionDelay="0.15s"
+            <Flex
+              gap={12}
+              position="relative"
+              overflow="hidden"
+              css={{
+                "& > a": {
+                  visibility: { base: "hidden", md: "visible" },
+                  transition: "all",
+                  transitionDuration: "1s",
+                  transform: isAtTop() ? `translateY(0%)` : `translateY(-150%)`,
+                  transitionTimingFunction: "cubic-bezier(.86,0,.07,1)",
+                },
+                "& > button": {
+                  transition: "all",
+                  transitionDuration: "1s",
+                  transform:
+                    // TODO: for some reason there is a bug where the menu button is not visible when the view position is recovered on reload
+                    isServer || (isAtTop() && !isMobile())
+                      ? `translateY(150%)`
+                      : `translateY(0%)`, // This is than not applied while isAtTop() is false
+                  transitionTimingFunction: "cubic-bezier(.86,0,.07,1)",
+                },
+              }}
             >
-              EXPERIENCE
-            </panda.a>
-            <panda.a
-              ref={headerLinks[2]}
-              href="#contact"
-              onclick={() => lenis?.scrollTo("#contact", { lerp: 0 })}
-              transitionDelay="0.3s"
-            >
-              CONTACT
-            </panda.a>
-            <panda.a
-              ref={headerLinks[2]}
-              href="/resume"
-              transitionDelay="0.45s"
-            >
-              RESUME
-            </panda.a>
+              <panda.a
+                ref={headerLinks[0]}
+                href="#about"
+                onclick={() => lenis?.scrollTo("#about", { lerp: 0 })}
+              >
+                ABOUT
+              </panda.a>
+              <panda.a
+                ref={headerLinks[1]}
+                href="#experience"
+                onclick={() => lenis?.scrollTo("#experience", { lerp: 0 })}
+                transitionDelay="0.15s"
+              >
+                EXPERIENCE
+              </panda.a>
+              <panda.a
+                ref={headerLinks[2]}
+                href="#contact"
+                onclick={() => lenis?.scrollTo("#contact", { lerp: 0 })}
+                transitionDelay="0.3s"
+              >
+                CONTACT
+              </panda.a>
+              <panda.a
+                ref={headerLinks[2]}
+                href="/resume"
+                transitionDelay="0.45s"
+              >
+                RESUME
+              </panda.a>
 
-            <panda.button position="absolute" right="0" transitionDelay="0.45s">
-              • MENU
-            </panda.button>
+              <panda.button
+                position="absolute"
+                right="0"
+                transitionDelay="0.45s"
+                onclick={() => setMenuOpen(true)}
+                cursor="pointer"
+              >
+                • MENU
+              </panda.button>
+            </Flex>
           </Flex>
-        </Flex>
-      </WideContainer>
-    </panda.div>
+        </WideContainer>
+      </panda.div>
+    </>
   );
 };
---
+++ src/components/HeaderMenu.tsx
@@ -0,0 +1,146 @@
+import { lenis } from "src/constants/lenis";
+import { Container, Flex, panda } from "styled-system/jsx";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+export const HeaderMenu = (props: Props) => {
+  return (
+    <panda.div
+      position="fixed"
+      zIndex={100}
+      top="0"
+      right="0"
+      left={{ base: "0", md: "unset" }}
+      height="100vh"
+      transform={
+        props.isOpen ? "translateX(0%)" : "translateX(calc(100% + 6vw))"
+      }
+      transition="all .85s cubic-bezier(.7, 0, .2, 1)"
+    >
+      <panda.div
+        position="absolute"
+        width={props.isOpen ? "1vw" : "6vw"}
+        height="100%"
+        top="0"
+        left="0"
+        zIndex={-1}
+        transition="all .85s cubic-bezier(.7, 0, .2, 1)"
+      >
+        <panda.div
+          height="150%"
+          content=""
+          display="block"
+          position="absolute"
+          backgroundColor="slate.800"
+          width="750%"
+          left="50%"
+          borderRadius="50%"
+          transform="translate(-20%,-16.33%)"
+          zIndex="1"
+          shadow="2xl"
+        />
+      </panda.div>
+      <Flex
+        height="100%"
+        flexDirection="column"
+        justifyContent="space-between"
+        padding="15vh 7.5vw 10vh 7.5vw"
+        backgroundColor="slate.800"
+      >
+        <panda.button
+          position="absolute"
+          top="0"
+          right="min(4vw, 4rem)"
+          paddingTop="12"
+          color="slate.200"
+          onclick={props.onClose}
+          cursor="pointer"
+        >
+          • CLOSE
+        </panda.button>
+        <Flex
+          flexDirection="column"
+          textAlign="left"
+          css={{
+            "& a": {
+              color: "slate.200",
+              textAlign: "left",
+              fontSize: "5xl",
+              transition: "all",
+              transitionDuration: "1s",
+              transitionTimingFunction: "cubic-bezier(.7, 0, .2, 1)",
+              transform: props.isOpen ? "translateX(0)" : "translateX(20rem)",
+              "&:hover": {
+                color: "slate.400",
+              },
+            },
+          }}
+        >
+          <panda.h3
+            color="slate.400"
+            paddingBottom="4"
+            marginBottom="10"
+            borderBottom="1px solid"
+            borderBottomColor="slate.200"
+          >
+            NAVIGATION
+          </panda.h3>
+          <panda.a
+            href="#about"
+            onclick={() => {
+              props.onClose();
+              lenis?.scrollTo("#about", { lerp: 0 });
+            }}
+          >
+            ABOUT
+          </panda.a>
+          <panda.a
+            href="#experience"
+            onclick={() => {
+              props.onClose();
+              lenis?.scrollTo("#experience", { lerp: 0 });
+            }}
+            transitionDelay="0.05s"
+          >
+            EXPERIENCE
+          </panda.a>
+          <panda.a
+            href="#contact"
+            onclick={() => {
+              props.onClose();
+              lenis?.scrollTo("#contact", { lerp: 0 });
+            }}
+            transitionDelay="0.1s"
+          >
+            CONTACT
+          </panda.a>
+          <panda.a href="/resume" transitionDelay="0.15s">
+            RESUME
+          </panda.a>
+        </Flex>
+        <Flex
+          flexDirection="column"
+          paddingTop="4"
+          borderTopWidth="1px"
+          borderTopStyle="solid"
+          borderTopColor="slate.100"
+        >
+          <panda.div fontSize="sm" color="slate.400">
+            Socials
+          </panda.div>
+          <Flex gap="10" color="slate.200">
+            <panda.a href="https://instagram.com/spaaacetoast/">
+              Instagram
+            </panda.a>
+            <panda.a href="https://linkedin.com/in/richard-boomsma">
+              Linkedin
+            </panda.a>
+            <panda.a href="https://github.com/spaaacetoast">GitHub</panda.a>
+          </Flex>
+        </Flex>
+      </Flex>
+    </panda.div>
+  );
+};
