45b480a3666e2638b800c6c35f46f417810d21b2
Richard Boomsma <spaaacetoast@proton.me>
2024-08-02 15:04:05 +0200

    removed panda astro integration, deprecated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
-import pandacss from "@pandacss/astro";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://obscure.software",
-  integrations: [pandacss(), solidJs()]
+  integrations: [solidJs()]
 });
